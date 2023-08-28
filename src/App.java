79610b8e4861d744bb5aaaf6dbd857bd7f4fbf3b
AbeKhan <128449118+AbeKhan@users.noreply.github.com>
2023-08-28 15:32:53 -0500

    Fixed toString and some other things

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+import javax.swing.text.html.HTMLDocument.HTMLReader.FormAction;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception 
+    {
+      Fraction test = new Fraction();
+      test.toString();
     }
 }
