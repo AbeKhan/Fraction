daacf9bdb921896b3910a8dde38ed8c45c48f201
AbeKhan <128449118+AbeKhan@users.noreply.github.com>
2023-08-30 15:25:50 -0500

    Added the add, muiltply, reduce, lcm, and gcd methods, and finshed them.

--- src/App.java
+++ src/App.java
@@ -3,7 +3,12 @@ import javax.swing.text.html.HTMLDocument.HTMLReader.FormAction;
 public class App {
     public static void main(String[] args) throws Exception 
     {
-      Fraction test = new Fraction();
-      test.toString();
+      Fraction test = new Fraction(2,9);
+      Fraction test2 = new Fraction(2, 5);
+      System.out.println("testing toString for test: " + test);
+      System.out.println("testing toString for test2: " + test2);
+      System.out.println(test + " + " + test2 + " equals " + test.add(test2));
+      System.out.println(test + " x " + test2 + " equals " + test.multiply(test2));
     }
 }
+
